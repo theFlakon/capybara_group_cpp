08ceba36fd80e10c1bb1e2d4d10f79bd519b4c94
Sergey Krylosov <seankrylosov@edu.hse.ru>
2026-02-18 17:55:59 +0300

    added main

--- students_proj/app/main.cpp
+++ students_proj/app/main.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+#include <fstream>
+#include "output.hpp"
+#include "group.hpp"
 
 int main() {
-    std::cout << "ТЕ";
+    std::ifstream input("data.txt");
+    Group new_group;
+    bool group = makeGroup(input, new_group);
+
+    outputGroupInfo(new_group);
+    return 1;
 }
