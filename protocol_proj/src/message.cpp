1a7bcd159dc3e5d7904cceafb726c9cb91c07a87
Ilia Kugaevskii <q89841439361@gmail.com>
2026-04-22 07:09:13 +0300

    Added message class to protocol project

--- protocol_proj/src/message.cpp
+++ protocol_proj/src/message.cpp
@@ -1,1 +1,40 @@
 #include "message.hpp"
+
+Message::Message(std::string version, std::string type,
+                 std::variant<TextPayload, BinaryPayload> payload)
+    : _version(std::move(version)), _type(std::move(type)),
+      _payload(std::move(payload))
+{
+}
+
+const std::string& Message::getVersion() const
+{
+    return _version;
+}
+
+const std::string& Message::getType() const
+{
+    return _type;
+}
+
+const std::variant<TextPayload, BinaryPayload>& Message::getPayload() const
+{
+    return _payload;
+}
+
+std::string Message::transformStrBin() const
+{
+    auto binPayload = std::get_if<BinaryPayload>(&_payload);
+    std::string resultStr{};
+
+    if(!binPayload)
+    {
+        auto textPayload = std::get_if<TextPayload>(&_payload);
+        return textPayload->text;
+    }
+
+    for(size_t idx = 0; idx < binPayload->data.size(); ++idx)
+        resultStr += static_cast<char>(binPayload->data[idx]);
+
+    return resultStr;
+}
