7f70847eb36b27568b0d2435deb29cee5e1be3fb
harun00000 <ut14kutsko41@gmail.com>
2026-04-22 15:32:12 +0300

    added queue hpp cpp

--- protocol_proj/include/message_queue.hpp
+++ protocol_proj/include/message_queue.hpp
@@ -4,7 +4,6 @@
 #include <condition_variable>
 #include <mutex>
 #include <queue>
-#include <optional>
 
 class MessageQueue
 {
@@ -12,16 +11,17 @@ private:
     std::queue<Message> _queue;
     size_t _maxCapacity;
 
-    std::mutex _mtx;                            // for that several threads do not climb in queue at the same time
-    std::condition_variable _condVar;           // 
+    mutable std::mutex _mtx;                            // for that several threads do not climb in queue at the same time
+    std::condition_variable _condVar;           
 
 public:
-    explicit MessageQueue(size_t maxCapacity);  // to create object explicitly (явно)
+    explicit MessageQueue(size_t maxCapacity);          // to create object explicitly (явно)
     ~MessageQueue();
 
-    void push(const Message &msg);
-    std::optional<Message> pop();               // or object exists or no (std::nullopt)
+    void push(Message msg);
+    Message pop();               
 
     bool empty() const;
     size_t size() const;
 };
+
--- protocol_proj/src/message_queue.cpp
+++ protocol_proj/src/message_queue.cpp
@@ -1,35 +1,48 @@
 #include "message_queue.hpp"
-#include "message.hpp"
-#include <mutex>
 
 MessageQueue::MessageQueue(size_t maxCapacity) : 
     _maxCapacity(maxCapacity == 0 ? 1 : maxCapacity) {}
 
-MessageQueue::~MessageQueue(){
-    while (!empty()) {
-        pop();
-    }
+MessageQueue::~MessageQueue() = default;
+
+bool MessageQueue::empty() const{
+    std::unique_lock<std::mutex> lock(_mtx);
+    return _queue.empty();
 }
 
-bool MessageQueue::empty() const {
-    if (size() == 0) return true;
-    return false;
-}
-
-size_t MessageQueue::size() const {
+size_t MessageQueue::size() const{
+    std::unique_lock<std::mutex> lock(_mtx);
     return _queue.size();
 }
 
-void MessageQueue::push(const Message &msg) {
-    // close access
-    std::lock_guard<std::mutex> lock(_mtx);
+void MessageQueue::push(Message msg){
+    // close other streams, we can change only queue
+    std::unique_lock<std::mutex> lock(_mtx);
 
     // wait when there will be free place in queue
     _condVar.wait(lock, [this]()
     {
         return _queue.size() < _maxCapacity;
+        // if false -> unlock mutex, sleep (wait other stream, notify), lock, check again
+        // if true -> push :)
     });
 
-    _queue.push(msg);
-    lock.unlock();
+    _queue.push(std::move(msg));    // don't copy
+    _condVar.notify_one();            // if someone waits -> wake
+}
+
+Message MessageQueue::pop(){
+    std::unique_lock<std::mutex> lock(_mtx);
+
+    _condVar.wait(lock, [this]{
+        return !_queue.empty();
+        // if false -> unlock mutex, sleep (wait other stream, notify), lock, check again
+        // if true -> continue
+    });
+
+    Message msg = std::move(_queue.front());
+    _queue.pop();
+    _condVar.notify_one();
+
+    return msg;
 }
