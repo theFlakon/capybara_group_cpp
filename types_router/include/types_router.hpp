b1108365c1c2425c98e15b119a139174721ddfe8
Ilia Kugaevskii <q89841439361@gmail.com>
2026-05-27 10:09:56 +0300

    Added comments to the old code

--- types_router/include/types_router.hpp
+++ types_router/include/types_router.hpp
@@ -3,17 +3,22 @@
 
 #include <cstddef>
 
+// Forward declarations of the class and struct templates
 template <typename T_> class TypesRouter;
 template <typename T_> struct TypeImplement;
 
+// Explicit template specialization of TypeImplement for the 'int' type
 template <> struct TypeImplement<int>
 {
+    // Compilet ime constants representing metadata for 'int'
     static constexpr const char* NAME = "int";
     static constexpr std::size_t ID = 1;
 
+    // Static function to handle type-specific logic for 'int'
     static void process(TypesRouter<int>& obj, int value);
 };
 
+// Generic class template that stores type metadata and a value
 template <typename T_> class TypesRouter
 {
 private:
@@ -22,28 +27,38 @@ private:
     T_ _value{};
 
 public:
+    // Constructor that accepts an initial value
     explicit TypesRouter(T_ value);
 
+    // Setters
     void setTypeIdx(std::size_t typeIdx)
     {
         _typeIdx = typeIdx;
     }
+
     void setTypeName(const char* typeName)
     {
         _typeName = typeName;
     }
+
     void setValue(T_ value)
     {
         _value = value;
     }
 };
 
+// Definition of the constructor for the generic TypesRouter template
 template <typename T_> TypesRouter<T_>::TypesRouter(T_ value)
 {
-    setValue(value);
+    setValue(value);  // Initialize the internal value
+
+    // Delegate metadata assignment and value processing to the TypeImplement
+    // struct.
     TypeImplement<T_>::process(*this, value);
 }
 
+// Definition of the process function for the 'int' specialization
+// Inline key word is mandatory here
 inline void TypeImplement<int>::process(TypesRouter<int>& obj, int value)
 {
     obj.setTypeIdx(ID);
